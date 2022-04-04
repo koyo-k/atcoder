c7ba24c782b86114b949180dd05c321bdb8cd2a4
kou-practice <kouyou.kuroshita@artemis-tech.js>
2022-04-04 22:55:13 +0900

    gitignore適用

---
+++ Rust/target/rls/debug/deps/rust-e42aa8b97e066851.d
@@ -0,0 +1,5 @@
+/home/koyo/rust_competitive_programing/atcoder/Rust/target/rls/debug/deps/rust-e42aa8b97e066851.rmeta: src/main.rs
+
+/home/koyo/rust_competitive_programing/atcoder/Rust/target/rls/debug/deps/rust-e42aa8b97e066851.d: src/main.rs
+
+src/main.rs:
