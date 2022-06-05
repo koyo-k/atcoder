5eadd4b566e7f9e44822a476d4240acd6552445d
kou-practice <kouyou.kuroshita@artemis-tech.js>
2022-06-05 20:26:21 +0900

    fix conflict

--- Rust/src/bin/abc191_b.rs
+++ Rust/src/bin/abc191_b.rs
@@ -3,13 +3,8 @@ use proconio::input;
 
 fn main() {
     input! {
-<<<<<<< HEAD
         n: usize, x: i64,
         mut a: [i64;n],
-=======
-        n: usize, x: i128,
-        mut a: [i128;n],
->>>>>>> b7b9eddd7d14da367a395b042afa4c04f9d46b52
     };
     println!("{}", a.into_iter().filter(|a| *a != x).join(" "));
 }
