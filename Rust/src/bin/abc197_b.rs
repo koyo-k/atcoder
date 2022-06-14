b4a2aa30604261489da0b0cb796e4a9eb4e49434
kou-practice <kouyou.kuroshita@artemis-tech.js>
2022-06-14 23:03:15 +0900

    2022/06/14

---
+++ Rust/src/bin/abc197_b.rs
@@ -0,0 +1,37 @@
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
+
+fn main() {
+    input! {
+        h: usize, w: usize, x: Usize1, y: Usize1,
+        s: [Chars; h],
+    };
+    let mut ans = 1;
+    for i in (0..x).rev() {
+        if s[i][y] == '#' {
+            break;
+        }
+        ans += 1;
+    }
+    for i in x + 1..h {
+        if s[i][y] == '#' {
+            break;
+        }
+        ans += 1;
+    }
+    for i in (0..y).rev() {
+        if s[x][i] == '#' {
+            break;
+        }
+        ans += 1;
+    }
+    for i in y + 1..w {
+        if s[x][i] == '#' {
+            break;
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
+}
